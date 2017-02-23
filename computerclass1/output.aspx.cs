b2d6d1d0d7f50a126fec4339c537915f19827cda
qcmiao1998 <qcmiao@hotmail.com>
2017-02-23 20:08:20 +0800

    first version

---
+++ computerclass1/output.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace computerclass1
+{
+    public partial class output : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = Server.HtmlEncode( "茶叶");
+            Response.Write("<script language='javascript'>alert('欢迎光临茶叶网站')</script>");
+        }
+    }
+}
