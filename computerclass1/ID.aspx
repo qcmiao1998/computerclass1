1241f6218fcd1fed05415053ab41e3a158bdf3df
qcmiao1998 <qcmiao@hotmail.com>
2017-03-02 20:40:26 +0800

    身份证第一版，正则式判断有问题

---
+++ computerclass1/ID.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ID.aspx.cs" Inherits="computerclass1.ID" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            请输入身份证号码：<asp:TextBox ID="TextBox1" runat="server" Width="297px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="确认" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ computerclass1/ID.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace computerclass1
+{
+    public partial class ID : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String ID = TextBox1.Text;
+            //判断长度合法
+            if (ID.Length != 18)
+            {
+                Returnfunc(1);
+                return;
+            }
+            //判断有无非数字或末尾X
+            if (Regex.IsMatch(ID,@"^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$") is false)
+            {
+                Returnfunc(2);
+                return;
+            }
+
+
+        }
+        protected void Returnfunc (int back)
+        {
+            switch (back)
+            {
+                case 1: Response.Write("<script language='javascript'>alert('输入的位数错误')</script>");break;
+                case 2: Response.Write("<script language='javascript'>alert('该身份证号码格式错误')</script>"); break;
+
+            }
+
+        }
+    }
+}
