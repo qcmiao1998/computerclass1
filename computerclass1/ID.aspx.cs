8b62aca3a6a493b78fdb8d57d4f398dbdc833482
qcmiao1998 <qcmiao@hotmail.com>
2017-03-03 12:57:35 +0800

    将身份证号码判断过程独立为一个类

--- computerclass1/ID.aspx.cs
+++ computerclass1/ID.aspx.cs
@@ -17,30 +17,15 @@ namespace computerclass1
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            String ID = TextBox1.Text;
-            //判断长度合法
-            if (ID.Length != 18)
+            IDClass1 vid = new IDClass1();
+            string outputwords = "Unknown ERROR";
+            switch (vid.VerifyID(TextBox1.Text))
             {
-                Returnfunc(1);
-                return;
-            }
-            //判断有无非数字或末尾X
-            if (Regex.IsMatch(ID,@"^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$") == false)
-            {
-                Returnfunc(2);
-                return;
-            }
-
-
-        }
-        protected void Returnfunc (int back)
-        {
-            switch (back)
-            {
-                case 1: Response.Write("<script language='javascript'>alert('输入的位数错误')</script>");break;
-                case 2: Response.Write("<script language='javascript'>alert('该身份证号码格式错误')</script>"); break;
+                case 1: outputwords= "输入的位数错误"; break;
+                case 2: outputwords= "该身份证号码格式错误"; break;
 
             }
+            Response.Write("<script language='javascript'>alert('"+ outputwords +"')</script>");
 
         }
     }
---
+++ computerclass1/IDClass1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace computerclass1
+{
+    public class IDClass1
+    {
+        public int VerifyID(string ID)
+        {
+            //判断长度合法
+            if (ID.Length != 18)
+            {
+                return 1;
+            }
+            //判断有无非数字或末尾X
+            if (Regex.IsMatch(ID, @"^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$") == false)
+            {
+                return 2;
+            }
+
+
+            return 0;
+        }
+    }
+}
