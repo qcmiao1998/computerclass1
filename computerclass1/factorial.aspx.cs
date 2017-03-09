7bf484ce53604f295ee3046cc8aee529a12efa1f
qcmiao1998 <qcmiao@hotmail.com>
2017-03-09 19:55:39 +0800

    阶乘 alpha

---
+++ computerclass1/factorial.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace computerclass1
+{
+    public partial class factorial : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            long outnum = 1;
+            for (int i = 1; i <= int.Parse(TextBox1.Text); i++)
+            {
+                outnum = outnum * i;
+            }
+            Response.Write(outnum.ToString());
+        }
+    }
+}
