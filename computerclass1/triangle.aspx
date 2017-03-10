ba6be4b86d614c7cd4dab477526cabd435919fb6
qcmiao1998 <qcmiao@hotmail.com>
2017-03-10 21:36:44 +0800

    字母三角形alpha

---
+++ computerclass1/triangle.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="triangle.aspx.cs" Inherits="computerclass1.triangle" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Output Triangle" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ computerclass1/triangle.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace computerclass1
+{
+    public partial class triangle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int num = new int();
+            try
+            {
+                num = int.Parse(TextBox1.Text);
+            }
+            catch
+            {
+                Response.Write("<script language='javascript'>alert('请输入1到26的整数！')</script>");
+                return;
+            }
+            if (num <= 0 || num > 26)
+            {
+                Response.Write("<script language='javascript'>alert('请输入1到26的整数！')</script>");
+                return;
+            }
+            Response.Write("<tt>");
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 0; j < num - i; j++)
+                {
+                    Response.Write("&ensp;");
+                }
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    char word = (char)(64 + i);
+                    Response.Write(word);
+                }
+                Response.Write("</br>");
+            }
+            Response.Write("</tt>");
+        }
+    }
+}
